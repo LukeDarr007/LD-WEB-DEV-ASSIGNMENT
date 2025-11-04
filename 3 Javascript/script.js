e2be65c7395375b89e7271845892d020c1accf5c
LukeDarr007 <c5024327@hallam.shu.ac.uk>
2025-11-04 18:33:29 +0000

    changes
    changes

---
+++ 3 Javascript/script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.querySelector.SelectorAll(".image-slider");
+
+    sliders.ForEach(slider =>{
+        let index = 0;
+        const slides = slider.querySelectorAll(".slide img");
+        const totaal = slides.length;
+
+        slides.forEach((img, i) => {
+            img.style.display = i === 0 ? "block" : "none";
+        });
+
+        function showSlides(n) {
+            slides.forEach((img, i) => {
+                img.style.display = i === n ? "block" : "none"; 
+            });
+        }
+
+        const prev = slider.querySelector(".prev");
+        const next = slider.querySelector(".next");
+
+        prev.addEventListener("click", () => {
+            index = (index - 1 + total) % total;
+            showSlide(index);
+        });
+
+        next.addEventListener("click", () => {
+            index = (index + 1) % total;
+            showSlides(index);
+        });
